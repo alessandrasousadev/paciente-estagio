4bbf5d25aff06d90e2b754caa106870da71262f5
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-05-31 21:37:22 -0300

    inicia o projeto

---
+++ src/main/java/br/com/kroton/pacienteestagio/PacienteEstagioApplication.java
@@ -0,0 +1,13 @@
+package br.com.kroton.pacienteestagio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacienteEstagioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacienteEstagioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kroton/pacienteestagio/PacienteEstagioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kroton.pacienteestagio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacienteEstagioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
