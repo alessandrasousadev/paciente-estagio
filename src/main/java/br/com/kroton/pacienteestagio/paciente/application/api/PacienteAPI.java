d9491aa9309b4318c0ac99b52ffbe230c17b015a
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-06-13 20:00:06 -0300

    implementa contrato da API

---
+++ src/main/java/br/com/kroton/pacienteestagio/paciente/application/api/PacienteAPI.java
@@ -0,0 +1,16 @@
+package br.com.kroton.pacienteestagio.paciente.application.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/paciente")
+public interface PacienteAPI {
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	PacienteResponse postPaciente(PacienteRequest pacienteRequest);
+}
---
+++ src/main/java/br/com/kroton/pacienteestagio/paciente/application/api/PacienteRequest.java
@@ -0,0 +1,5 @@
+package br.com.kroton.pacienteestagio.paciente.application.api;
+
+public class PacienteRequest {
+
+}
