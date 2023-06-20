ca9e2d3b04a777521c751c352a9da273e539ce13
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-06-20 10:58:39 -0300

    implementa PacienteRepository. Teste ok

--- src/main/java/br/com/kroton/pacienteestagio/paciente/infra/PacienteInfraRepository.java
+++ src/main/java/br/com/kroton/pacienteestagio/paciente/infra/PacienteInfraRepository.java
@@ -4,15 +4,18 @@ import org.springframework.stereotype.Repository;
 
 import br.com.kroton.pacienteestagio.paciente.application.repository.PacienteRepository;
 import br.com.kroton.pacienteestagio.paciente.domain.Paciente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class PacienteInfraRepository implements PacienteRepository {
-
+	private final PacienteSpringDataJPARepository pacienteSpringDataJPARepository;
 	@Override
 	public Paciente salva(Paciente paciente) {
 		log.info("[inicia] PacienteInfraRepository - salva");
+		pacienteSpringDataJPARepository.save(paciente);
 		log.info("[finaliza] PacienteInfraRepository - salva");
 		return paciente;
 	}
